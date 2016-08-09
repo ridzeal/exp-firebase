5c51ddc80d5ee5a866612856f326a57196122536
Riadhi Aditya <ridzeal@gmail.com>
2016-08-09 23:18:18 +0700

    Add firebase config

---
+++ src/js/firebase.config.example.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+    apiKey: "apiKey",
+    authDomain: "projectId.firebaseapp.com",
+    databaseURL: "https://databaseName.firebaseio.com",
+    storageBucket: "bucket.appspot.com",
+};
+firebase.initializeApp(config);
